a85788287b7cff10915dfb69d8be1c450fc553a9
B2Bomber2 <ferrariguan@HP-Laptop-15-MacOS.local>
2025-05-07 18:07:01 -0700

    added ultrasonic sensor pins

--- main.ino
+++ main.ino
@@ -1,11 +1,20 @@
-
+// first motor
 const unsigned int in1{36};
 const unsigned int in2{48};
 const unsigned int EnA{35};
 
+// second motor
 const unsigned int in3{34};
 const unsigned int in4{47};
 const unsigned int EnB{33};
+
+// first ultra sonic sensor
+const unsigned int trig1{26};
+const unsigned int echo1{21};
+
+// second ultra sonic sensor
+const unsigned int trig2{37};
+const unsigned int echo2{38};
 
 void setup(){
     pinMode(in1, OUTPUT);
@@ -16,10 +25,14 @@ void setup(){
     pinMode(in4, OUTPUT);
     pinMode(EnB, OUTPUT);
 
+    pinMode();
+    pinMode();
 
+    pinMode();
+    pinMode();
 }
 
 void loop(){
 
-    
+
 }
