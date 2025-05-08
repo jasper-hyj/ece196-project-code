8e5b1764794fa6cf625e31135b07f7807a5c6a8d
B2Bomber2 <ferrariguan@HP-Laptop-15-MacOS.local>
2025-05-07 18:25:07 -0700

    declared all the pins for every component

--- main.ino
+++ main.ino
@@ -1,28 +1,35 @@
 #include <Wire.h>
 // first motor
-const unsigned int in1{36};
-const unsigned int in2{48};
-const unsigned int EnA{35};
+const unsigned int in1 = 36;
+const unsigned int in2 = 48;
+const unsigned int EnA = 35;
 
 // second motor
-const unsigned int in3{34};
-const unsigned int in4{47};
-const unsigned int EnB{33};
+const unsigned int in3 = 34;
+const unsigned int in4 = 47;
+const unsigned int EnB = 33;
 
 // third motor
-const unsigned int in5{12};
-const unsigned int in6{10};
-const unsigned int EnC{11};
+const unsigned int in5 = 12;
+const unsigned int in6 = 10;
+const unsigned int EnC = 11;
 
 // first ultra sonic sensor
-const unsigned int trig1{26};
-const unsigned int echo1{21};
+const unsigned int trig1 = 26;
+const unsigned int echo1 = 21;
 
 // second ultra sonic sensor
-const unsigned int trig2{37};
-const unsigned int echo2{38};
+const unsigned int trig2 = 37;
+const unsigned int echo2 = 38;
+
+// used for calculating distance for ultra sonic sensor
+long duration;
+float distanceCm;
+
 
 void setup(){
+    Serial.begin(115200);
+
     pinMode(in1, OUTPUT);
     pinMode(in2, OUTPUT);
     pinMode(EnA, OUTPUT);
@@ -35,11 +42,11 @@ void setup(){
     pinMode(in6, OUTPUT);
     pinMode(EnC, OUTPUT);
 
-    pinMode();
-    pinMode();
+    pinMode(trig1, OUTPUT);
+    pinMode(echo1, INPUT);
 
-    pinMode();
-    pinMode();
+    pinMode(trig2, OUTPUT);
+    pinMode(echo2, INPUT);
 }
 
 void loop(){
