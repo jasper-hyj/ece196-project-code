f336f9e7a6d7013245dfab3ea5bb43d27fb73265
B2Bomber2 <ferrariguan@HP-Laptop-15-MacOS.local>
2025-05-07 17:53:35 -0700

    added the main arduino file with pin declarations

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # ece196-project-code
+
+
---
+++ main.ino
@@ -0,0 +1,25 @@
+
+const unsigned int in1{36};
+const unsigned int in2{48};
+const unsigned int EnA{35};
+
+const unsigned int in3{34};
+const unsigned int in4{47};
+const unsigned int EnB{33};
+
+void setup(){
+    pinMode(in1, OUTPUT);
+    pinMode(in2, OUTPUT);
+    pinMode(EnA, OUTPUT);
+
+    pinMode(in3, OUTPUT);
+    pinMode(in4, OUTPUT);
+    pinMode(EnB, OUTPUT);
+
+
+}
+
+void loop(){
+
+    
+}
