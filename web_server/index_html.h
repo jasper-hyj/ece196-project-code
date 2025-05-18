7d38fcf1492e221e808f1c2a8e6b033720767eb3
Jasper Huang <jasper.hyj@gmail.com>
2025-05-18 11:52:32 -0700

    Add web_server sample

---
+++ web_server/index_html.h
@@ -0,0 +1,8 @@
+const char index_html[] = R"rawliteral(
+    <!DOCTYPE html>
+    <html>
+      <head><meta name="viewport" content="width=device-width, initial-scale=1"></head>
+      <body><h1>Hello from header file</h1></body>
+    </html>
+    )rawliteral";
+    
